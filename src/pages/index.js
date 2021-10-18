c595302096408a33a551bc72664c469730aad498
Reza Azerly <kiritosao080@gmail.com>
2021-10-18 11:31:58 +0700

    index js changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BrandStats from '../components/brand-stats'
 import Catalogue from '../components/catalogue'
+import Header from '../components/header'
 
 export default function IndexPage() {
   return (
     <div>
+      <Header />
       <BrandStats />
       <Catalogue />
     </div>

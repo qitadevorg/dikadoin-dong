4ea137f070eddf4104fb09707b66271193317186
Albarra Erdanto <albarraerdanto@gmail.com>
2021-10-18 10:48:12 +0700

    fix centering brand stats

--- src/components/brand-stats.js
+++ src/components/brand-stats.js
@@ -4,22 +4,22 @@ export default function BrandStats() {
   return (
     <section className="px-5 py-8 bg-brand-brown text-brand-yellow">
       <div className="max-w-4xl mx-auto flex items-center">
-        <div className="flex-1">
-          <p className="text-center">
+        <div className="flex-1 flex justify-center">
+          <p>
             <span className="text-4xl">15+</span><br/>
             Variasi Produk
           </p>
         </div>
         <hr className="bg-brand-yellow h-14 w-1 rounded-full" />
-        <div className="flex-1">
-          <p className="text-center">
+        <div className="flex-1 flex justify-center">
+          <p>
             <span className="text-4xl">96+</span><br/>
             Produk Terjual
           </p>
         </div>
         <hr className="bg-brand-yellow h-14 w-1 rounded-full" />
-        <div className="flex-1">
-          <p className="text-center">
+        <div className="flex-1 flex justify-center">
+          <p>
             <span className="text-4xl">80+</span><br/>
             Pelanggan
           </p>
